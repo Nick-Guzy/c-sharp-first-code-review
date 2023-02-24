71adfe4f8cfc8c9d09583eaa019ee67394d02e78
NicksFed <nick.guzy76@gmail.com>
2023-02-24 10:10:40 -0800

    Set up framework for all files

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -1,16 +1,7 @@
-namespace ShapeTracker.Models
+namespace Bakery.Models
 {
-	public class Ellipse
+	public class Pastry
 	{
-		private int _rad1;
-		private int _rad2;
-public int Rad1 { get; set; }
-public int Rad2 { get; }
-
-		public Ellipse(int rad1, int rad2)
-		{
-			Rad1 = rad1;
-      Rad2 = rad2;
-    }
+		
 	}
 }
--- Pastry.Tests/ModelTests/PastryTests.cs
+++ Pastry.Tests/ModelTests/PastryTests.cs
@@ -1,46 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ShapeTracker.Models;
+using Bakery.Models;
 
-namespace ShapeTracker.Tests
+namespace Bakery.Tests
 {
 	[TestClass]
-	public class EllipseTests
+	public class PastryTests
 	{
 		[TestMethod]
-		public void EllipseConstructor_CreatesInstanceOfEllipse_Ellilpse()
+		public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
 		{
-			Ellipse newEllipse = new Ellipse(2, 3);
+			Pastry newPastry = new Pastry();
 		}
 
-		public void GetRadius1_ReturnsRadius1_int() 
-		{
-			// Arrange
-      int rad1 = 3;
-      Ellipse newEllipse = new Ellipse(rad1, 4);
-			// Act
-      int result = newEllipse.Rad1;
-			// Assert
-      Assert.AreEqual(rad1, result);
-    }
-		public void SetRad1_SetsValueOfRad1_Void()
-    {
-      // Arrange
-      Ellipse newEllipse = new Ellipse(3, 4);
-      int newRad1 = 44;
-      // Act
-      newEllipse.Rad1 = newRad1;
-      // Assert
-      Assert.AreEqual(newRad1, newEllipse.Rad1);
-    }
-		public void GetRad2_ReturnsRad2_Int()
-    {
-      //Arrange
-      int rad2 = 3;
-      Ellipse newEllipse = new Ellipse(2, rad2);
-      //Act
-      int result = newEllipse.Rad2;
-      //Assert
-      Assert.AreEqual(rad2, result);
+		
     }
 	}
-}
