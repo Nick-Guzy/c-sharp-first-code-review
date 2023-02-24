fa2cddb7caf9354e0f73c527490a8099a3ec00ba
Nicholas Guzy <119460594+NicksFed@users.noreply.github.com>
2023-02-23 22:05:21 -0800

    Initial commit

---
+++ ShapeTracker/Models/Rectangle.cs
@@ -0,0 +1,28 @@
+// using System.Collections.Generic;
+
+namespace ShapeTracker.Models 
+{
+	public class Rectangle
+	{
+		private int _side1;
+		public int Side1
+		{
+			get { return _side1; }
+      set { _side1 = value; }
+    }
+		public int Side2 { get; set; }
+
+
+    public Rectangle(int length1, int length2)
+		{
+      _side1 = length1;
+      Side2 = length2;
+    }
+		public int GetArea()
+		{
+      return (_side1 * Side2);
+    }
+
+
+  }
+}
